89a96196e0fed370392168a864837470cf82a8e6
Hanan Ashraf <hanan.ashraf.abdo@gmail.com>
2025-06-01 13:22:16 +0300

    Completed Task 4: Concatenate two arguments

---
+++ 4-concat.js
@@ -0,0 +1,8 @@
+// Get the first argument (process.argv[0] is node, process.argv[1] is the script file)
+const arg1 = process.argv[2];
+
+// Get the second argument
+const arg2 = process.argv[3];
+
+// Print the arguments in the specified format
+console.log(arg1 + " is " + arg2);
